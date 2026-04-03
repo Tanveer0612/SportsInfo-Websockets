3826c637812b0364c2e4af77af8199ec42032e96
Tanveer Singh <bhamratanveer00612@gmail.com>
2026-04-03 19:34:08 +0530

    Implement database schema

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import connectDB from "./db/db.js";
 
 const app = express();
-const port = 8000;
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.get('/', (req, res) => {
     res.send("Hello From express server!");
 })
 
-app.listen(port, () =>{
-    console.log(`App is running at http://localhost:${port}`);
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.error("Server error:", error);
+        process.exit(1);
+    })
+    
+    const PORT = process.env.PORT || 8080;
+
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+
+})
+.catch((error) => {
+    console.log("MongoDB Connection failed !!!", error);
 })
